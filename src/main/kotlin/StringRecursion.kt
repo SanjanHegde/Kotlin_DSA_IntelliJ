eb72bdffc3fb8d8750dcb8e6b09a49f5dc021525
Sanjan Hegde <sanjanhegde051@gmail.com>
2023-11-11 23:43:52 +0530

    Print Reverse of a string

---
+++ src/main/kotlin/StringRecursion.kt
@@ -0,0 +1,17 @@
+/**
+ * Question 1
+ * Print Reverse of a string without using any extension functions or loops
+ * Solution: Use Recursion
+ * Author: Sanjan Hegde
+ **/
+
+fun reverseString(string: String): String {
+    return if (string.isEmpty()) string
+    else reverseString(string.substring(1)) + string[0]
+}
+
+fun main() {
+    val sentence = "Sanjan Hegde"
+    val reversed = reverseString(sentence)
+    println("The reversed sentence is: $reversed") // Output: edgeH najnaS
+}
